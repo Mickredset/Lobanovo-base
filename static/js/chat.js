1af5d828137a207d6ee1e8a85b3251218edb09a8
Mickredset <aivliev782@gmail.com>
2026-01-07 20:20:03 +0300

    Create chat.js

---
+++ static/js/chat.js
@@ -0,0 +1,7 @@
+const socket = io();
+
+const messageForm = document.getElementById('message-form');
+const messageInput = document.getElementById('message-input');
+const messagesDiv = document.getElementById('messages');
+
+messageForm.addEventListener('
